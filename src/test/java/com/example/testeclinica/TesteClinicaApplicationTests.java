6681bd44ffe8ff699b9b65ece43cb39ffe6428b3
José L. F. Silva <jose.silva1463@etec.sp.gov.br>
2021-01-29 12:58:26 -0300

    Projeto criado

---
+++ src/test/java/com/example/testeclinica/TesteClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeclinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
